615b6637b36842f071d79cf6b36882536b36c90f
deviduttanayak-github <nayak.devidutta17@gmail.com>
2021-02-21 18:03:24 +0530

    text-sharing added

--- lan-screenshare/server/index.js
+++ lan-screenshare/server/index.js
@@ -20,6 +20,8 @@ app.get("/", (req, res, next) => {
 });
 
 var buff = new Queue(10); // TODO
+var rooms = [];     var ping = 50;
+var last_visit = 0;
 
 // --------- internal-img-data-transport ---------
 var stream_itr = 1; var str_data = "", sdata = "";
@@ -31,10 +33,8 @@ var header_length = 10; // header_length
 client.on('data', function(data) {
     str_data = String.fromCharCode.apply(null, data);
     head = str_data.substring(0,header_length);
-    // console.log("Head : ", head);
     if(head.substring(0,1) === "#"){
         buff.push_data(sdata);
-        // console.log("Pushed :", typeof(sdata), " size : ", sdata.length);
         sdata = str_data.substring(header_length, str_data.length);
     }
     else{
@@ -51,17 +51,42 @@ setInterval(() => {
 }, 1);
 
 // -------- Web socket Protocol with client ---------
-var ping = 50;
 
 soi.on("connection", (socket) => {
     console.log("SID : ", socket.id);
     socket.on("client-data", (args) => {
-        // console.log("R: ", args);
-    });
-    setInterval(() => {
         socket.emit("server-data", buff.get_last_data());
-    }, ping);
+        last_visit = new Date().getTime();
+    });
+
+    // --------- web socket Protocol with client - text channel -------
+    socket.on("create-room", (room_id) => {
+        var rid =  room_id.split("#")[0];
+        // console.log("RID : ", rid);
+        socket.join(rid);
+        rooms.push([rid, socket.id]);
+        socket.emit("create-room-res", "done");
+    });
+    socket.on("join-req", (token)=> { 
+        var f = true;
+        for( var i=0; i<rooms.length; i++){
+            if(rooms[i][0] == token){
+                socket.join(token);
+                f = false;  break;
+            }
+        }        
+        if(f) socket.emit("forwarded-text", "[SERVER] : No such room with this ID exists.");
+    });
+    socket.on("forward-text", (packet) => {
+        let data_indx = packet.split("#")[0].length + 1; 
+        let msg = packet.substring(data_indx, packet.length );
+        let room = packet.split("#")[0];
+        // console.log("Room : ", room); 
+        // console.log("Data : ", msg );        
+        soi.sockets.in(room).emit("forwarded-text", msg);
+    });
 });
+
 
 server.listen(port);
 console.log("Listening on port : ", port);
--- lan-screenshare/server/public/src.js
+++ lan-screenshare/server/public/src.js
@@ -1,39 +1,160 @@
-// ---------- toggler part --------------
+var img_conn = false, rceived = false, txt_conn = false;
+var last_changed = 0;
+var meta_data = "data:image/png;base64,";
 var loc = "home";  // screen-viewer
+var last_loc = "home";
+var token = "";  
+var can_share = false;
+
+// ---------- toggler part --------------
 document.querySelector('#boom').onclick = (e) => {
-    console.log("clicked");
+    last_loc = loc;
     document.getElementById("home").style.display = 'none';
     document.getElementById("screen-viewer").style.display = 'block';
+    document.getElementById("text-share").style.display = 'none';
     loc = "screen-viewer";
-}
+};
+document.querySelector('#boom-text').onclick = (e) => {
+    last_loc = loc;
+    document.getElementById("home").style.display = 'none';
+    document.getElementById("screen-viewer").style.display = 'none';
+    document.getElementById("text-share").style.display = 'block';
+    loc = "text-share";
+};
 
 // todo: go back button
-
+document.querySelector("#back-btn").onclick = (e) => {
+    last_loc = loc;
+    document.getElementById("home").style.display = 'flex';
+    document.getElementById("screen-viewer").style.display = 'none';
+    document.getElementById("text-share").style.display = 'none';
+    loc = "home";
+}
 // ---------- toggler part -------------- end
 
-// var test = [2];
-// test[0] = "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO";
-// test[1] = "9TXL0Y4OHwAAAABJRU5ErkJggg==";
-
-var meta_data = "data:image/png;base64,";
-var img_data = "iVBORw0KGgoAAAANSUhEUgAAAGAAAAA2CAIAAAC3LQuFAAAM20lEQVR4nN1baVRT1xb+zk1IgIAhgAOgIEWsohXnAQVERftEcUAFpbW1trpczjNOrVal+qxT9Nn3+jrZStWlVetQa6n6tOJQbesAaOsIVaYUGQOZ7j3vxw2XGAIkMVCW38qPc/bZ+5x9d/beZ7jnEtQHd3f3srKyetkciNDQ0Bs3bji2z169eplWr169aqUgA2DPpiQzarDEuZqDYWqKRYw+blol9Q3zKCvLtCqVSs0YKKV1tNbEoamDTKvLV62oV8Q+EDPTmkL1V1EDjdoU0NxbYQ2bWCiFhYVdvHgxMjLy3LlzphwLt+1uVa6Vqk88qAg498m3im6eMoOmGM2CA10BHNp7NmZiFOfW+se0P1SZVwZ3Czz9W363wbFadXlIc7VWlXfs8m3HPpgDEbJ7XeYbK7+YOubNTw8fPnJo5N7Pl/l32LRpU54arWRVuYXajpfGJ9khZRHpZXW1Ths0qI5W1qBJP5JsRiwt16or9RVafaXWoNEZtDpWq2e1etbAcizL2apeSUkJoSbB/wKgUqMnDEMIGGJMjIQQwPiMYpGFfFoHSktLrRKoAIImLAMwPrqHUqkszD4PIGjCsrg35/AMZ3YtBJB3QQlgxKSFoGxGOcqBqFfjlErl0eTpeYBSqbygXPbBtxkAru1eXQwolcqjmRX3zu4B8OtX7yLnwsJ1ytVv9ACwYvcVAEeTp09ftBI5F5RKJS+lvJDHl2sDR7GnZ+DNLatPjx9yYUK4OvO8av976p8+s8kupnCwB1FKG3lNYIbC4gqJRCxiCMMQ3nsIeA8iAJU4iWzqrbS01MEGYlm2d+8+iuVni5KjauMRi8UGgwGAq6trRUWFHaOEDhjcwts99cj3PtDkAiFdOmpKuQePfncGNIBH84D+XdxPnE4HENKuZea9fLsfh2GYBjGQAzv82yGun0UApwVT/xKuKjmitLQsJiambRtQp8BdO3bWxq/Vavwnfai4sqhr3767Uy4D8G4X8de983K5XOBp37lzqAsOXE0HIBvwtvrCJxNHdN97/Ne6NdFotFY8lWVsXBC7dMtR2JaDdE8h8aybhWXZPn36DIoadObsmdJSq5KRVquxSDc1EACO41iWtaZDsVhMCMHzGUiAzSHGsmx5eXlNukQicXFx4Q3EU3gDffR+nEdw3MSkf+3dMHPixEk1BXkPyv560fxpb239+DSQVZMHgF6vj4uLu3TpknE4dy9dWWFNNi9nye3sx4TUu/mxFlYZSKOp/pPLysp2Hv5lzbRXTSnu7u63bt0KDg5mWTYyMpKn2+RBLp6tKp/mCcRWQf2lTsi6k7Zr7fK0O1kpKSl6vd7X19eaDgsKChxoINsWTjxMrQPA3d3djCE6oretfRLyjCZ599Oy7qT9M7F7BZsRFajbuHqRk5OTSqVSqVQDpycnDe05LbKTvl3YwOnJQ5O2qlSqgSGynkOTVG16qlSqCS972fFQAnw9fJ5RzA4Pat68+cvt28tcxIXFalmzFvHjRgAYO3asRQ/ynfddzrYpkEvHRfc6ePCbmp3zHuTaqmNF3jO7tk4RMRl3/+gU3HH062PWvzPF5ge1F74K95yiat+3ZRZ7FoXFagDq0oK62a5UlrRB/rjoOIvWEWBmHQAZ508AyMi9m3H+qN1K2oGglp5ihhGLRA8KnsIOD6qsrLx161ZNntatW/v6+tqdg5osrPIgZ2dn03J4eHhNHj4vWjkTN2UMHTr0hx9+EKr2JGliCfXIhA4G8MH7cbFh/hbbneQt+UKwSYr0UHglhL1ih4bPg/Sfb/n4VCthj4FsxZ6VDG6cBrDs3W+OXsy2yBPQobOxYACceojbTQCwanifIF/vRtDQFG29A3Jzc4VqgxioRYc+ZV4dhOpr67h6Re5pjD74owrQ/+Lmpo4YmfC4+9gu0YO6A1V/qM/kpTt69uvZPaqXpwKdPOAdEelw5VkvGtCutVC1eSWtoSxzPk0S3pdCDIOeikS3b9/hN2BHDqTEjp045S3jlNwQSTphZMS+Y+dtUthWuDYPqFBVr+Zt9iAxy4ryc/Vz5mH5isqECWBEUqmri7NUJpNNeuMdV5mrKXNA31GZ/5VC7g9gw9R/PL/2DW0dAD6u3PDhw4WqzR5UUlLCFziOS03NnTnruKpgyeTJk2fMmNGrV6+TJ0+uWbOGZ+A9aPLcpV9u31hHh018mrffQDDZcD958sTb21sqlRoMhr59+/JEh4dY0uT+G75Ms0VZB8D+lbQAljP4nZgCgILCUjqWdYnx0haKRc4PCrOR/8DugRrfOnCIgUBE3NsnGYbJy3mqaOGKT8KFQ1UjGFGPtihnNQ+eGGrrw/+ViOxb5vklMTExJSUtalxiGC7eLis8dOqmA7S1ETaHWHFxsbAsNDvTQtWBGV+2O8QUCkVRUVN5qWvzLObAo5ba0HSsAwcuFHlPtOiPsq6x/UfOB7B61SxHDddocMwsplKp+B2ZwWCIiYnhiUKIiT3bGJ7+WVuHTXyad4wHeXorLl4o8PT0VMgtXJmowzp1IMDHFxIZX96279jG1YsSh3eP8EFiQmxESFuBLXFLil0qWwtHzGKAiIhjR4fo9XrO4jxvF7TObtDlANKEsPbzEkYCSAiRR4THOLtyKZmPBLbo1uzt4YlP84tmd8DClO9q9hMViLMP7VfDMSEmwOIsJusa27VNUGzbJ5UeLVavNX9BVhViToDeJk0aB47xIEppHbOb+vrRtOuob5HXUNbx97d8AiUgO9vyCQwPm3NQbbf7UlNTtVptenq6rR02PkbM3GY9s80h9ujRI4XCmImFEDMYDGq1OicnRyQSTZpkfDtYHWJdYry0hW6+XpmZt2tuNfgQ44KG6EZEOm9fxRO5frOZSztqUaEb8FtdKioiUXSuLgZAEz3MOfWUUNVFJ4Atk5w5AY92ho7dODcRAKfUfcRRC0WxWCyXyzt27BgUFGRBhhH1aIuXubvQ1LrVYO7/6Lx9lS46gf+7mEs7dL27VDX6PMv7jHV00QmV6z42/ZN13dwAcEFDNHPXAtD368/TNQuWCDxUIdMs2iVUJan7JGdOAEDxPfGlA5LUfZLUffxz2pmkKSgBkTdrBlAQhuM4fh1kmqTFYlcAEDmBrSu/5OZafNfcalx8pIgtB7D/4In4+HgAf968fPG25RfTDQebk7ROp5NIJCIi4aieUoNeb/ju5KmAgAA/Pz9vb+9jx46ZC9RpHVMMi99war9wITnv4P79QtO1jMKenYzvS0fFxwNwBi5/vz+rxLwTh8NmD7p//763t/EgXS6Xcxx38ODBcePGbd68efHixc+1WZUHoKSxHaRe2JyDcrLT5bIK/tf5lRCGzZ8wJpxh8xfPew36XOhz7NelJIsPJaBV95jZAIaM4qsYFr9B4Irf8O9uw+Kt6U8XnQBA36mdTVpooodBuPXp8DuKdnvQizOLWQPh1BXAl1vmR01dXK/IizOL1Qblto1z5i019SCfjj1+v/K/egWb+G7eWgOtfD163Vep5tTyDLh1MiWwLCt+97qDdGsSsDbELFgHMLMOqpaRPyW+ZM4pN74vWz24+pbYwNbO5myNiwFdW9TeaNTtRfsUwYE4depUv379HLObN4NKpTp//XqQohV1k4oMOimlj/LyBn2wDUe/IE5uhVqNPL9Q8sX+iqQ5Zn9Ozdt8fzsaxEAlJSVuHJdfmINC4ym1Tqfj3k9ySlqD2XNa6isr12/gPv/015+vmQlGREQ0hD7PA7Ju804+0Ex2oRTCFw4AIYSjHIykqovilFaXecbqHoher3/48KF/gD8oNFqNh4dHVSATXohSEMIPiqoQf+ZEiVKYNAkioJRyikCeyBQZDwqJUX8A0Ol0tOoLSF6cIeAohfBZpKn+PJXyhGcsQCnEYpHBYCAufiEVjzNkbToLyYhSSggDAkrpygUz12/dBaAiNcw1+iJAXBYeHhWz7OuBmZqFf+WFd2k7Jm/++l1ig/bDNQu8D4WSa6qC9TkARnx4/fiiroSQypM3XWNCKaVL/LduzJoPoIMn+b0IlFLNipsuyaEA7q6bG7xyO6WUcQKnByGEi3zdf/T0x/MHPLhyquDy5b5z35sQ1fX4sWNqmd+8BUuXTH61Zdxa0b0zqJoWCCGUcib6EwCUo5RSEUMALJ8/I3nrR4SQ5fNnrN+yi+PYVQtnJW/9KCjQP350DID1W3YJPQCofJLp4hdCCCGurTvxe3FT63Acx4gYfqTq8SgFCBFLqUEr0DkKwpB5S5anfPofVWGh4CYtmnsVqKovehOGUEopBcMwYm2xQepBKfXzcH5cVMkwjPDKyGQgo/MSQjiONbkkTPWBA/mS+P7ZKgZO+CjMqBXHMQxDOcoQ826rPZejhBD+WKKqCQAC2vhl/fkEwPi3Zh34bOf/AVA95RAmrO1tAAAAAElFTkSuQmCC";
-
-const displayImg = () => {
-    document.getElementById("screen").src = meta_data+img_data ;
-}
-
-// displayImg();
-
 // ------------- socket io part ---------------
+
+setInterval(() => {
+    if(loc !== last_loc && loc === "screen-viewer" ) socket.emit("client-data", "Hi!! from Client-Side");
+}, 1000);
+
 const socket = io();
 socket.on("reconnect_attempt", () => {
-    console.log("socket");
+    img_conn = true;
+    socket.emit("client-data", "Hi!! from Client-Side"); // test
 });
-
 socket.emit("client-data", "Hi!! from Client-Side");
 socket.on("server-data", (img_data ) => {
-    console.log("R : ", typeof(img_data));
+    img_conn = true;
     document.getElementById("screen").src = meta_data+img_data ;
-})
+    if(loc === "screen-viewer")
+    socket.emit("client-data", "Hi!! from Client-Side");
+});
+socket.on('disconnect', (reason) => {
+    img_conn = false;
+});
+
+setInterval(() => {
+    if(img_conn) {
+        document.getElementById("status-dot").style.backgroundColor = "#9DF05C";
+        document.getElementById("status").innerHTML = "connected";
+        document.getElementById("status-dot-txt").style.backgroundColor = "#9DF05C";
+        document.getElementById("status-txt").innerHTML = "connected";
+    }
+    else{
+        document.getElementById("status-dot").style.backgroundColor = "red";
+        document.getElementById("status").innerHTML = "connecting...";
+        document.getElementById("status-dot-txt").style.backgroundColor = "red";
+        document.getElementById("status-txt").innerHTML = "connecting...";
+    }
+    if(can_share) document.getElementById("incoming").innerHTML = "Sharing";
+    else if(rceived) document.getElementById("incoming").innerHTML = "Viewing Shared text";
+}, 1000);
 
 // ------------- socket io part --------------- end --
+
+// ------------- text-editor ------------------
+/*
+function onKeyUp(e){
+    //detect 'tab' key
+    if(e.keyCode == 9){
+        //add tab
+        console.log("Tab-pressed");
+        document.getElementById("text-box-left").innerHTML += '&#009';
+        //prevent focusing on next element
+        e.preventDefault();   
+    }
+}
+*/
+// source : stack-overflow
+function onKeyDown(e) {
+    var curr = new Date().getTime();
+    if(curr - last_changed >= 2000) socket.emit("forward-text", buildMsg(token, document.getElementById("text-box-left").innerHTML));
+
+    last_changed = new Date().getTime();
+    // console.log("T : ", last_changed)
+    if (e.keyCode === 9) { // tab key
+        e.preventDefault();  // this will prevent us from tabbing out of the editor
+        // now insert four non-breaking spaces for the tab key
+        var editor = document.getElementById("text-box-left");
+        var doc = editor.ownerDocument.defaultView;
+        var sel = doc.getSelection();
+        var range = sel.getRangeAt(0);
+
+        var tabNode = document.createTextNode("\u00a0\u00a0\u00a0\u00a0");
+        range.insertNode(tabNode);
+
+        range.setStartAfter(tabNode);
+        range.setEndAfter(tabNode); 
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+}
+const buildMsg = (tok, txt) => {
+    return (tok + "#" + txt);
+};
+
+document.querySelector("#text-setter").onclick = (e) => {
+    var size = document.getElementById("txt-size").value;
+    var color =  document.getElementById("txt-color").value;
+    document.getElementById("text-box-left").style.fontSize = size+"rem";
+    document.getElementById("text-box-left").style.color = color;
+}
+
+document.querySelector("#share-txt").onclick = (e) => {
+    token = document.getElementById("token-text-share").value;
+    socket.emit("create-room", token);
+    setTimeout(() => {
+        socket.emit("forward-text", buildMsg(token, " " ));
+    }, 1000);
+};
+
+document.querySelector("#join-txt").onclick = (e) => {
+    socket.emit("join-req", document.getElementById("token-text").value);
+    can_share = false;
+}
+
+socket.on("create-room-res", (res) => {
+    if(res == "done"){
+        can_share = true;
+    }
+    else can_share = false;
+    // console.log("Res : ", res, " ", can_share);
+});
+
+socket.on("forwarded-text", (data) =>{
+    // console.log("D: ", data);
+    rceived = true;
+    if(!can_share)
+    document.getElementById("text-box-left").innerHTML = data;
+    else 
+    setTimeout(() => {
+        var curr = new Date().getTime();
+        if( curr-last_changed < 2000)
+        socket.emit("forward-text", buildMsg(token, document.getElementById("text-box-left").innerHTML));
+    }, 500);
+});
+// setInterval(() => {
+//     if(can_share)
+//     socket.emit("forward-text", document.getElementById("text-box-left").innerHTML);
+// }, 500);
